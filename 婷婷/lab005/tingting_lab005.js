8c89a326b1a59dcf6aaf311e2746f23a1fa8542f
唐婷婷 <ft2017@protonmail.com>
2017-03-27 11:33:31 +0800

    婷婷新增lab005js\bpmn等

--- 婷婷/lab005/tingting_lab005.js
+++ 婷婷/lab005/tingting_lab005.js
@@ -63,6 +63,21 @@ function btn4_onclick(){
      
 }
 
+function btn5_onclick(){
+    // alert("btn2_onclick");
+  	var FileName = "SingleOpenWin";		
+	var sql = " select UNIQUE( a.pmaa001) 供应商编号,b.pmaal003 供应商全名,TO_CHAR(a.pmaacnfdt, 'YYYY-MM-DD') 资料审核日期  from pmaa_t a left join pmaal_t b on b.pmaal001=a.pmaa001 where a.pmaa002=1 order by a.pmaa001"; 
+	// 单号	生产料号	品名	本站作业	作业名称	生产数量	预计完工日
+
+	var SQLClaused = new Array(sql);
+	var SQLLabel = new Array("供应商编号"," 供应商全名","资料审核日期");//客制开窗的Grid Label
+	var QBEField = new Array("供应商编号"," 供应商全名","资料审核日期");//模糊查询,須和DB Table栏位名称相同
+	var QBELabel = new Array("供应商编号"," 供应商全名","资料审核日期");//模糊查询的Label
+    var ReturnId = new Array("txte1","txte2","txte3");//表单上的栏位代号
+	singleOpenWin(FileName, ds2, SQLClaused, SQLLabel, QBEField, QBELabel, ReturnId, 720, 430);
+     
+}
+
 
 function formCreate(){
 return true;
